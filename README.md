c318a949d925307e4304a9f6673dd95fe267585c
zhixun-luy <luys8611@gmail.com>
2022-09-29 03:43:16 +0800

    working for auto trading - interact with order service via rest api

--- README.md
+++ README.md
@@ -2,45 +2,40 @@
 
 ## Install dependencies
     opencv-python
-    python-dotenv
     requests
     mss
-`python-dotenv` is for **_slack-notification_**. For slack-notification, please create `.env` file.
+    pillow
+    pyqt5
+<!--  
+`python-dotenv` is for **_slack-notification_**. For slack-notification, please create `.env` file. -->
 
 ## How to run
-Check the connection of camera.
-After installation of opencv, please run main.py.
+First, you must run api server. After then, please run main.py.
 
 Here, argument **_show_** is for displaying the detection result.
 
-    usage: main.py [-h] [-i INPUT_TYPE] [-c CONFIG] [-v VIDEO] [--show] [--log_file LOG_FILE]
-
+    usage: main.py [-h] [-i INPUT_TYPE] [--show] [--trade]
+    
     optional arguments:
-    -h, --help            show this help message and exit
-    -i INPUT_TYPE, --input_type INPUT_TYPE
-                            input type to be processed. (default: capture)
-    -c CONFIG, --config CONFIG
-                            config file path of ticker (default: ticker.config)
-    -v VIDEO, --video VIDEO
-                            Path for video file to be processed when input type is video. (default: None)
-    --show                Showing process and result frame. (default: False)
-    --log_file LOG_FILE   log file path (default: None)
+        -h, --help            show this help message and exit
+        -i INPUT_TYPE, --input_type INPUT_TYPE
+                                input type to be processed. (default: capture)
+        --show                Showing process and result frame. (default: False)
+        --trade               Decide if auto trade is enable or not. (default: False)
 
-### Camera
-```commandline
-python main.py --camera --show
-```
-In case that several cameras are available, Please select one of them. Default camera index is 0.
- 
-### Video
-If you need to process the video file, please point the video path.
-```commandline
-python main.py --video "path/to/video" --show
-```
+
 ### Screen Capture
+
 In case of screen capture, Please use the below command. If so, the available ticker names will be displayed from config file. From there, select one of them and select the specific area on screeen.
 ```commandline
-python main.py -i capture -c ticker.config
+python main.py -i capture --show --trade
 ```
 
 https://user-images.githubusercontent.com/96384530/184595640-47954005-1199-4cfd-bf85-2fa601dcf57a.mp4
+
+### UI Manual 
+```
+python main_app.py
+```
+By using combobox, please select the target Ticker config file. 
+Click _**FLATTEN**_, **_REVERSE_**, **_BUY_**, **_SELL_** button and place order action. 
--- main.py
+++ main.py
@@ -1,49 +1,81 @@
 import os
 import argparse
 from datetime import datetime
+import time
+from urllib import request
 
 import cv2
-from camera_utils import check_camera_idx
 from process import LS_Detector
 from rect_input import rectangle_select
 from capture import ScreenCap
-from trade import process_trade
-from log_utils import export_log
-import configparser
-
-log_folder = "log"
-os.makedirs(log_folder, exist_ok=True)
-
-trade_config_dir = "order_utils/cfg"
-os.makedirs(trade_config_dir, exist_ok=True)
-global_trade_file = "order_utils/TRADE_FLAG.cfg"
-
-
-def get_global_flag():
-    if os.path.exists(global_trade_file):
-        try:
-            config = configparser.ConfigParser()
-            config.read([global_trade_file])
-            return config.getboolean("global", "global")
-        except:
-            return False
-    else:
-        print("Global Trade File not exists.")
-        return False
+# from trade import process_trade
+# from log_utils import export_log
+# import configparser
+# import json
+import requests
 
 
 def get_config_file_list():
+    data = {
+        "command": "TICKERS",
+        "data": None
+    }
+    tick_list = []
+    try:
+        rep_data = requests.get("http://127.0.0.1:8088/config", json=data)
+        print(rep_data)
+        if rep_data.status_code == 200:
+            json_data = rep_data.json()
+            if json_data["status"] == "success":
+                tick_list = json_data["message"][0]
+            else:
+                print("Failed to get config file list...\nTry again.")
+        else:
+            print(request.text)
+        return tick_list
+    except ConnectionRefusedError as err:
+        print("Failed to access: {}".format(repr(err)))
+        return []
 
-    files = [file for file in os.listdir(trade_config_dir) if file.endswith(".cfg")]
-    print(files)
-    return files
+
+def send_log(msg, trade_config_path, is_notified=False):
+    print(msg)
+    try:
+        log_data = {
+            "message": msg,
+            "is_notified": is_notified,
+            "config_file": trade_config_path
+        }
+        data = {
+            "command": "EXPORT_LOG",
+            "message": log_data
+        }
+        result = requests.post("http://127.0.0.1:8088/auto_order", json=data)
+        print(result.json()["message"][0])
+    except:
+        print("Failed to export log.")
+    
+def send_order(order_data: dict):
+
+    try:
+        data = {
+            "command": "PLACE_ORDER",
+            "message": order_data
+        }
+        result =  requests.post("http://127.0.0.1:8088/auto_order", json=data)
+        return result.json()["message"][0]
+
+    except:
+        return "Failed to send order request."
 
 
-def ls_detect(cap, is_show, log_file, ticker_name=None, is_trade=False, trade_config_path=None):
+def ls_detect(cap, trade_config_path, is_show, is_trade=False):
     # print("hello world!!!!")
-    if ticker_name is None:
-        ticker_name = "Test_Ticker"
-    export_log("{}\tStarted...".format(ticker_name), log_file, is_notified=True)
+
+    now = datetime.now().strftime("%m-%d-%Y %H:%M:%S.%f")[:-3]
+    msg = "{}\t{}\tScreen Capture Started...".format(now, trade_config_path)
+
+    send_log(msg, trade_config_path, is_notified=True)
     
     processor = LS_Detector()
     cur_signal = None
@@ -71,47 +103,52 @@ def ls_detect(cap, is_show, log_file, ticker_name=None, is_trade=False, trade_co
                         if cur_signal == "L":
                             full_signal = "LONG"
                             L_num += 1
-                            action = "BUY"
                         else:
                             full_signal = "SHORT"
                             S_num += 1
-                            action = "SELL"
                         msg = "{}\t{}".format(full_signal, L_num + S_num)
-                        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-                        tm_msg = '{}\t{}\t{}'.format(ticker_name, now, msg)
-                        export_log(tm_msg, log_file, is_notified=True)
+                        now = datetime.now().strftime("%m-%d-%Y %H:%M:%S.%f")[:-3]
+                        tm_msg = '{}\t{}\t{}'.format(trade_config_path, now, msg)
+                        send_log(tm_msg, trade_config_path, is_notified=True)
                         if is_trade:
-                            if get_global_flag():
-                                args_order = {
-                                    "contract": {},
-                                    "order": {
-                                        "signal": cur_signal,
-                                        "action": action
-                                    }
-                                }
-                                # print(f"{trade_config_path=}")
-                                result_order = process_trade(args_order, cfg_file=trade_config_path)
-                                # now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-                                # tm_msg = '{}\t{}\t{}'.format(ticker_name, now, result_order["description"])
-                                tm_msg = result_order["description"]
-                                slack_msg = result_order["slack_msg"]
-                                export_log(tm_msg, log_file, slack_msg, True)
-                            else:
-                                print("\tGlobal Trade Disabled...")
+                            order_data = {
+                                "signal": cur_signal,
+                                "ticker": trade_config_path
+                            }
+                            order_result = send_order(order_data)
+                            print("Order Result:\n{}".format(order_result))
+                            # if get_global_flag():
+                            #     args_order = {
+                            #         "contract": {},
+                            #         "order": {
+                            #             "signal": cur_signal,
+                            #             "action": action
+                            #         }
+                            #     }
+                            #     # print(f"{trade_config_path=}")
+                            #     result_order = process_trade(args_order, cfg_file=trade_config_path)
+                            #     # now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+                            #     # tm_msg = '{}\t{}\t{}'.format(ticker_name, now, result_order["description"])
+                            #     tm_msg = result_order["description"]
+                            #     slack_msg = result_order["slack_msg"]
+                            #     export_log(tm_msg, log_file, slack_msg, True)
+                            # else:
+                            #     print("\tGlobal Trade Disabled...")
 
                 if is_show:
                     cv2.imshow('frame', response_data["frame"])
             else:
                 msg = "Failed to Process: {}".format(response_data['descript'])
                 now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-                tm_msg = '{}\t{}\t{}'.format(ticker_name, now, msg)
-                export_log(tm_msg, log_file, is_notified=True)
+                tm_msg = '{}\t{}\t{}'.format(trade_config_path, now, msg)
+                send_log(tm_msg, trade_config_path, is_notified=True)
+
                 break
         else:
             msg = "Done process..."
             now = datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-            tm_msg = '{}\t{}\t{}'.format(ticker_name, now, msg)
-            export_log(tm_msg, log_file, is_notified=True)
+            tm_msg = '{}\t{}\t{}'.format(trade_config_path, now, msg)
+            send_log(tm_msg, trade_config_path, is_notified=True)
             break
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -124,47 +161,48 @@ def ls_detect(cap, is_show, log_file, ticker_name=None, is_trade=False, trade_co
 
 
 def main(args):
-    default_types = ["capture", "camera", "video"]
+    default_types = ["capture"]
     input_type = args.input_type
-    video_file = args.video
+    # video_file = args.video
     # ticker_config_file = args.config
     is_show = args.show
     is_trade = args.trade
 
     assert input_type in default_types
+    pre_defined_cfg_files = get_config_file_list()
+
+    if pre_defined_cfg_files == []:
+        print("Config file list is empty. Please check it.")
+        return
 
     if input_type == "capture":
-        pre_defined_cfg_files = get_config_file_list()
-        if pre_defined_cfg_files is not None and pre_defined_cfg_files != []:
             # with open(ticker_config_file, "r") as t:
             #     pre_defined_tickers = [tick.strip() for tick in t.readlines() if tick.strip() != ""]
             # if len(pre_defined_tickers) == 0:
             #     print("Empty config file. Please confirm that.")
             #     return
-            print("The existing ticker names are follow:")
-            for i, tick in enumerate(pre_defined_cfg_files):
-                print("\t{}:\t{}".format(i, tick))
-            
-            while True:
-                ticker_idx = input("Please select ticker index as integer: ")
-                try:
-                    ticker_idx = int(ticker_idx)
-                    if ticker_idx in range(len(pre_defined_cfg_files)):
-                        ticker_config_file = pre_defined_cfg_files[ticker_idx]
-                        # print()
-                        break
-                    else:
-                        print("Error: invalid ticker index. Please select again.")
-                except ValueError:
-                    print("This is invalid index type. Input integer number.")
-            if ticker_config_file is None:
-                return
-            
-        else:
-            print("No exists config files. Please check that.")
+        print("The existing ticker names are follow:")
+        for i, tick in enumerate(pre_defined_cfg_files):
+            print("\t{}:\t{}".format(i, tick))
+        
+        while True:
+            ticker_idx = input("Please select ticker index as integer: ")
+            try:
+                ticker_idx = int(ticker_idx)
+                if ticker_idx in range(len(pre_defined_cfg_files)):
+                    ticker_config_file = pre_defined_cfg_files[ticker_idx]
+                    # print()
+                    break
+                else:
+                    print("Error: invalid ticker index. Please select again.")
+            except ValueError:
+                print("This is invalid index type. Input integer number.")
+        if ticker_config_file is None:
             return
 
         print("Capture screen.. \nPlease confirm box boundaries and labels")
+
+        time.sleep(1)
         coordinates = rectangle_select()
         if coordinates is None:
             return
@@ -173,66 +211,24 @@ def main(args):
             ticker_info = "Ticker Name: {}\tTicker Coordinates: {}".format(ticker_name, coordinates)
             
             cap = ScreenCap(coordinates)
-            log_file = ticker_name + "_" + datetime.now().strftime("%m_%d_%Y") + ".log"
+            # log_file = ticker_name + "_" + datetime.now().strftime("%m_%d_%Y") + ".log"
 
-            log_path = os.path.join(log_folder, log_file)
-            export_log(ticker_info, log_path)
-            trade_config_path = os.path.join(trade_config_dir, ticker_config_file)
-            ls_detect(cap, is_show, log_path, ticker_name, is_trade, trade_config_path)
-
-    elif input_type == "video":
-        cap = cv2.VideoCapture(video_file)
-        log_file = args.log_file
-        if log_file is None:
-            log_file = datetime.now().strftime("%m_%d_%Y") + ".log"
-
-        log_path = os.path.join(log_folder, log_file)
-
-        ls_detect(cap, is_show, log_path)
-    elif input_type == "camera":
-        cam_idx = check_camera_idx()
-        # if log_file is None:
-        log_file = datetime.now().strftime("%m_%d_%Y") + ".log"
-        # cam_idx = [0]
-        if cam_idx == []:
-            export_log("\tThere is no available camera.", log_file)
-            return
-        elif len(cam_idx) != 1:
-            export_log("There are several available cameras. Please select one of them.\nAvailable Camera indices: {}".format(cam_idx),
-                       log_file)
-            while True:
-                idx = input("Please select camera: ")
-                try:
-                    idx = int(idx)
-                    if idx in cam_idx:
-                        cap = cv2.VideoCapture(idx, cv2.CAP_DSHOW)
-                        break
-                    else:
-                        print("Error: invalid camera index.")
-                except ValueError:
-                    export_log("This is invalid data type. Input integer number.", log_file)
-        else:
-            export_log("The connected camera index is {}".format(cam_idx[0]), log_file)
-            cap = cv2.VideoCapture(cam_idx[0], cv2.CAP_DSHOW)
-        
-        log_file = args.log_file
-        if log_file is None:
-            log_file = datetime.now().strftime("%m_%d_%Y") + ".log"
-
-        log_path = os.path.join(log_folder, log_file)
-
-        ls_detect(cap, is_show, log_path)
+            # log_path = os.path.join(log_folder, log_file)
+            # export_log(ticker_info, log_path)
+            # trade_config_path = os.path.join(trade_config_dir, ticker_config_file)
+            ls_detect(cap, ticker_config_file, is_show, is_trade)
 
     
 if __name__ == "__main__":
 
-    # default="chart line light Right to Left.mov"
+    default="chart line light Right to Left.mov"
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--input_type", default="capture", help="input type to be processed.")
     # parser.add_argument("-c", "--config", default="ticker.config", help="config file path of ticker")
 
-    parser.add_argument("-v", "--video", type=str, help="Path for video file to be processed when input type is video.")
+    # parser.add_argument("-v", "--video", type=str, help="Path for video file to be processed when input type is video.")
     parser.add_argument("--show", action="store_true", help="Showing process and result frame.")
-    parser.add_argument("--trade", action="store_true", help="process to trade")
+    parser.add_argument("--trade", action="store_true", help="Decide if auto trade is enable or not.")
     args = parser.parse_args()
     main(args)
+    # get_work_path()    
